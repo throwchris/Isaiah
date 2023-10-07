cc72e952771a80ac351d97e66c915d7ff2fcb884
throwchris <69865554+throwchris@users.noreply.github.com>
2023-10-07 18:19:44 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,289 @@
+function myFunction4() {
+  var element = document.getElementById("font");
+  if (element.classList === "w3-large") {
+    element.classList.toggle("w3-xlarge") 
+  } else {
+    element.classList.toggle("w3-xlarge")
+  }
+
+}
+
+var myAudio = document.getElementById("myAudio");
+var isPlaying = false;
+
+function togglePlay() {
+  isPlaying ? myAudio.pause() : myAudio.play();
+};
+
+myAudio.onplaying = function() {
+  isPlaying = true;
+};
+myAudio.onpause = function() {
+  isPlaying = false;
+}
+
+function myFunction2() {
+  var element = document.body;
+  element.classList.toggle("light-mode");
+}
+
+function Function1() {
+  var x = document.getElementById("myDIV1");
+  if (x.innerHTML === "********") {
+    x.innerHTML = "believed";
+  } else {
+    x.innerHTML = "********";
+  }
+}
+
+function Function2() {
+  var x = document.getElementById("myDIV2");
+  if (x.innerHTML === "******") {
+    x.innerHTML = "beauty";
+  } else {
+    x.innerHTML = "******";
+  }
+}
+function Function3() {
+  var x = document.getElementById("myDIV3");
+  if (x.innerHTML === "*******") {
+    x.innerHTML = "sorrows";
+  } else {
+    x.innerHTML = "*******";
+  }
+}
+
+function Function4() {
+  var x = document.getElementById("myDIV4");
+  if (x.innerHTML === "*****") {
+    x.innerHTML = "borne";
+  } else {
+    x.innerHTML = "*****";
+  }
+}
+
+function Function5() {
+  var x = document.getElementById("myDIV5");
+  if (x.innerHTML === "*******") {
+    x.innerHTML = "bruised";
+  } else {
+    x.innerHTML = "*******";
+  }
+}
+
+function Function6() {
+  var x = document.getElementById("myDIV6");
+  if (x.innerHTML === "******") {
+    x.innerHTML = "astray";
+  } else {
+    x.innerHTML = "******";
+  }
+}
+
+function Function7() {
+  var x = document.getElementById("myDIV7");
+  if (x.innerHTML === "********") {
+    x.innerHTML = "shearers";
+  } else {
+    x.innerHTML = "********";
+  }
+}
+
+function Function8() {
+  var x = document.getElementById("myDIV8");
+  if (x.innerHTML === "**********") {
+    x.innerHTML = "generation";
+  } else {
+    x.innerHTML = "**********";
+  }
+}
+
+function Function9() {
+  var x = document.getElementById("myDIV9");
+  if (x.innerHTML === "********") {
+    x.innerHTML = "violence";
+  } else {
+    x.innerHTML = "********";
+  }
+}
+function Function10() {
+  var x = document.getElementById("myDIV10");
+  if (x.innerHTML === "********") {
+    x.innerHTML = "pleasure";
+  } else {
+    x.innerHTML = "********";
+  }
+}
+
+function Function11() {
+  var x = document.getElementById("myDIV11");
+  if (x.innerHTML === "*******") {
+    x.innerHTML = "travail";
+  } else {
+    x.innerHTML = "*******";
+  }
+}
+
+function Function12() {
+  var x = document.getElementById("myDIV12");
+  if (x.innerHTML === "************") {
+    x.innerHTML = "intercession";
+  } else {
+    x.innerHTML = "************";
+  }
+}
+
+function Function13() {
+  var x = document.getElementById("myDIV13");
+  if (x.innerHTML === "*******") {
+    x.innerHTML = "married";
+  } else {
+    x.innerHTML = "*******";
+  }
+}
+
+function Function14() {
+  var x = document.getElementById("myDIV14");
+  if (x.innerHTML === "********") {
+    x.innerHTML = "curtains";
+  } else {
+    x.innerHTML = "********";
+  }
+}
+
+function Function15() {
+  var x = document.getElementById("myDIV15");
+  if (x.innerHTML === "*******") {
+    x.innerHTML = "inherit";
+  } else {
+    x.innerHTML = "*******";
+  }
+}
+
+
+function Function16() {
+  var x = document.getElementById("myDIV16");
+  if (x.innerHTML === "*******") {
+    x.innerHTML = "ashamed";
+  } else {
+    x.innerHTML = "*******";
+  }
+}
+
+function Function17() {
+  var x = document.getElementById("myDIV17");
+  if (x.innerHTML === "*******") {
+    x.innerHTML = "husband";
+  } else {
+    x.innerHTML = "*******";
+  }
+}
+
+function Function18() {
+  var x = document.getElementById("myDIV18");
+  if (x.innerHTML === "*******") {
+    x.innerHTML = "grieved";
+  } else {
+    x.innerHTML = "*******";
+  }
+}
+
+function Function19() {
+  var x = document.getElementById("myDIV19");
+  if (x.innerHTML === "*******") {
+    x.innerHTML = "mercies";
+  } else {
+    x.innerHTML = "*******";
+  }
+}
+
+function Function20() {
+  var x = document.getElementById("myDIV20");
+  if (x.innerHTML === "********") {
+    x.innerHTML = "kindness";
+  } else {
+    x.innerHTML = "********";
+  }
+}
+
+function Function21() {
+  var x = document.getElementById("myDIV21");
+  if (x.innerHTML === "*****") {
+    x.innerHTML = "sworn";
+  } else {
+    x.innerHTML = "*****";
+  }
+}
+
+function Function22() {
+  var x = document.getElementById("myDIV22");
+  if (x.innerHTML === "*******") {
+    x.innerHTML = "removed";
+  } else {
+    x.innerHTML = "*******";
+  }
+}
+
+function Function23() {
+  var x = document.getElementById("myDIV23");
+  if (x.innerHTML === "*******") {
+    x.innerHTML = "tempest";
+  } else {
+    x.innerHTML = "*******";
+  }
+}
+
+function Function24() {
+  var x = document.getElementById("myDIV24");
+  if (x.innerHTML === "*******") {
+    x.innerHTML = "windows";
+  } else {
+    x.innerHTML = "*******";
+  }
+}
+
+function Function25() {
+  var x = document.getElementById("myDIV25");
+  if (x.innerHTML === "******") {
+    x.innerHTML = "taught";
+  } else {
+    x.innerHTML = "******";
+  }
+}
+
+function Function26() {
+  var x = document.getElementById("myDIV26");
+  if (x.innerHTML === "***********") {
+    x.innerHTML = "established";
+  } else {
+    x.innerHTML = "***********";
+  }
+}
+
+function Function27() {
+  var x = document.getElementById("myDIV27");
+  if (x.innerHTML === "****") {
+    x.innerHTML = "sake";
+  } else {
+    x.innerHTML = "****";
+  }
+}
+
+function Function28() {
+  var x = document.getElementById("myDIV28");
+  if (x.innerHTML === "*******") {
+    x.innerHTML = "destroy";
+  } else {
+    x.innerHTML = "*******";
+  }
+}
+
+function Function29() {
+  var x = document.getElementById("myDIV29");
+  if (x.innerHTML === "********") {
+    x.innerHTML = "heritage";
+  } else {
+    x.innerHTML = "********";
+  }
+}
+
